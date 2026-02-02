d984b1805083d4772afe292ec50346f761d7e9f7
JoshSald <42002468+JoshSald@users.noreply.github.com>
2026-02-02 14:16:59 +0100

    Add main menu loop with basic navigation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("==== Budget Tracker ====");
+    Console.WriteLine("1) Add transaction");
+    Console.WriteLine("2) Remove transaction");
+    Console.WriteLine("3) Report");
+    Console.WriteLine("0) Exit");
+    Console.Write("Select: ");
+
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("Add transaction (coming soon)");
+            break;
+
+        case "2":
+            Console.WriteLine("Remove transaction (coming soon)");
+            break;
+
+        case "3":
+            Console.WriteLine("Report (coming soon)");
+            break;
+
+        case "0":
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+
+    Console.WriteLine("\nPress any key...");
+    Console.ReadKey();
+}
